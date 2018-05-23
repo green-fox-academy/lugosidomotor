17ead1ddf395b5c7d8efb433a804483799050809
lugosidomotor <lugosi.domotor@gmail.com>
2018-05-23 13:08:32 +0200

    postIt done

---
+++ week-03/day-03/postIt/src/PostIt.java
@@ -0,0 +1,16 @@
+public class PostIt {
+
+    String backgroundColor;
+    String text;
+    String textColor;
+
+
+    public PostIt (String backgroundColor, String text, String textColor) {
+
+        this.backgroundColor = backgroundColor;
+        this.text = text;
+        this.textColor = textColor;
+
+    }
+
+}
