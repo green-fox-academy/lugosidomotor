643cfc964f38e0f4696eec207716e8075f443c97
lugosidomotor <lugosi.domotor@gmail.com>
2018-05-24 13:31:55 +0200

    power done

---
+++ week-03/day-04/Recursion/src/Power.java
@@ -0,0 +1,19 @@
+public class Power {
+    public static void main(String[] args) {
+
+
+        int sumPower = powerFun(4,4);
+        System.out.println(sumPower);
+    }
+
+    private static int powerFun(int base, int n) {
+        if (n == 0) {
+            return 1;
+        } else
+            return (base * powerFun(base,n-1));
+    }
+
+
+}
+// Given base and n that are both 1 or more, compute recursively (no loops)
+// the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
