e7d2f7c180207d4722b44bcaa0b5770352863700
lugosidomotor <lugosi.domotor@gmail.com>
2018-05-30 16:34:00 +0200

    Zoo skeleton done, doesnt work yet

---
+++ week-04/day-03/Zoo/src/Animal.java
@@ -0,0 +1,18 @@
+public abstract class Animal {
+
+  private int age;
+  private String name;
+  private String gender;
+  private String color;
+  private String anyThing;
+
+  public String getName() {
+    return name;
+  }
+
+  public void breed() {
+  }
+
+
+  
+}
---
+++ week-04/day-03/Zoo/src/Mammal.java
@@ -0,0 +1,8 @@
+public class Mammal extends Animal{
+
+  public Mammal(String breed) {
+
+  }
+
+
+}
