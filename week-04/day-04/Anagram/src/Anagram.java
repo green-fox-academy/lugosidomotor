ef8db613cc7921a56974a24ac38c267585a5b485
lugosidomotor <lugosi.domotor@gmail.com>
2018-05-31 17:22:06 +0200

    Anagram final

--- week-04/day-04/Anagram/src/Anagram.java
+++ week-04/day-04/Anagram/src/Anagram.java
@@ -2,8 +2,6 @@ import java.util.Arrays;
 
 public class Anagram {
   public static void main(String[] args) {
-    System.out.println(IsAnagram("Lugosi Domotor","Dog Suitor Loom"));
-    System.out.println(anagramOld("Lugosi Domotor","Dog Suitor Loom"));
   }
 
   public static String sortString(String inputString) {
@@ -16,20 +14,20 @@ public class Anagram {
     return sortString(firstWord.replace(" ","").toLowerCase()).contains(sortString(secondWord.replace(" ","").toLowerCase()));
   }
 
-  public static boolean anagramOld(String stringOne, String stringTwo) {
-
-    if (stringOne.length() != stringTwo.length()) {
-      return false;
-    } else {
-      for (int i = 0; i <= stringOne.length(); i++) {
-        if (sortString(stringOne).charAt(i) == sortString(stringTwo).charAt(i)) {
-          return true;
-        }else{
-          return false;
-        }
-      }
-    }return true;
-  }
+//  public static boolean anagramOld(String stringOne, String stringTwo) {
+//
+//    if (stringOne.length() != stringTwo.length()) {
+//      return false;
+//    } else {
+//      for (int i = 0; i <= stringOne.length(); i++) {
+//        if (sortString(stringOne).charAt(i) == sortString(stringTwo).charAt(i)) {
+//          return true;
+//        }else{
+//          return false;
+//        }
+//      }
+//    }return true;
+//  }
 }
 
 
