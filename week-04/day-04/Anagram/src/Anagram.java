b492799bd57f9cf7031e01d69a6e8c7d4f396077
lugosidomotor <lugosi.domotor@gmail.com>
2018-05-31 16:49:55 +0200

    my beautiful Anagram code

--- week-04/day-04/Anagram/src/Anagram.java
+++ week-04/day-04/Anagram/src/Anagram.java
@@ -1,37 +1,22 @@
 import java.util.Arrays;
 
 public class Anagram {
-  public static void main(String[] args) {
 
-
-    System.out.println(anagram("diebbcdb","omed"));
-
-    }
-
-  public static String sortString(String inputString)  {
+  public static String sortString(String inputString) {
     char tempArray[] = inputString.toCharArray();
     Arrays.sort(tempArray);
     return new String(tempArray);
   }
 
-  public static boolean anagram(String stringOne, String stringTwo) {
-
-    String sortedOne = sortString(stringOne);
-    String sortedTwo = sortString(stringTwo);
-
-    if (stringOne.length() != stringTwo.length()) {
-      return false;
-    } else {
-      for (int i = 0; i <= sortedOne.length(); i++) {
-        if (sortedOne.charAt(i) == sortedTwo.charAt(i)) {
-          return true;
-        }else{
-          return false;
-        }
-      }
-    }return true;
+  public static boolean IsAnagram(String firstWord, String secondWord) {
+    return sortString(firstWord).contains(sortString(secondWord));
   }
 }
+
+
+
+
+
 
 
 
