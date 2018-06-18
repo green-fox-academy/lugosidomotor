7a6a419e34e932de73561362ab49cea95039e828
lugosidomotor <lugosi.domotor@gmail.com>
2018-06-18 11:57:10 +0200

    Exercise_4 done

---
+++ week-07/day-01/src/Exercise_4.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercise_4 {
+
+  public static void main(String[] args) {
+
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(3, 9, 2, 8, 6, 5));
+    System.out.println(getMoreThan20(numbers));
+    System.out.println(getMoreThan20WithStream(numbers));
+  }
+
+  public static ArrayList<Integer> getMoreThan20 (ArrayList<Integer> numbers){
+    ArrayList<Integer> square = new ArrayList<Integer>();
+    for (int i = 0; i < numbers.size(); i++) {
+      if (numbers.get(i) * numbers.get(i) > 20){
+        square.add(numbers.get(i));
+      }
+    }
+    return square;
+  }
+
+  public static List<Integer> getMoreThan20WithStream (ArrayList<Integer> numbers) {
+    return numbers.stream()
+            .filter(n -> n * n > 20)
+            .collect(Collectors.toList());
+  }
+}
