9591c7bd485f7abb1b2226db1ecca53d4155e930
lugosidomotor <lugosi.domotor@gmail.com>
2018-06-18 11:51:42 +0200

    Exercise_3 done

---
+++ week-07/day-01/src/Exercise_3.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercise_3 {
+
+  public static void main(String[] args) {
+
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+    System.out.println(getTheSquare(numbers));
+    System.out.println(getTheEvensWithStream(numbers));
+  }
+
+  public static ArrayList<Integer> getTheSquare (ArrayList<Integer> numbers){
+    ArrayList<Integer> square = new ArrayList<Integer>();
+    for (int i = 0; i < numbers.size(); i++) {
+      if (numbers.get(i) % 2 == 0){
+        square.add(numbers.get(i) * numbers.get(i));
+      }
+    }
+    return square;
+  }
+
+  public static List<Integer> getTheEvensWithStream (ArrayList<Integer> numbers) {
+    return numbers.stream()
+            .filter(n -> n % 2 == 0)
+            .map( n -> n * n)
+            .collect(Collectors.toList());
+  }
+}
